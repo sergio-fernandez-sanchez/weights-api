dbbbdc281f4d02d0ac4c9612c59209ae9b6f2f71
Dev <dev@MacBook-Pro-de-Sergio.local>
2026-05-09 16:07:56 +0200

    bug fixed

--- db/database.py
+++ db/database.py
@@ -4,10 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def get_connection():
+    """
+    Crea y devuelve una conexión a PostgreSQL usando las variables del .env.
+    """
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT", 5432)
     )
